9eed8533354e126e34ef77095b953c8d2b6f60d4
Frank Luongo <fluongo@oreilly.com>
2020-08-17 08:35:50 -0400

    CH2 Vid 3 | Loading Window File & Adding Icon

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ function createMainWindow() {
     width: 500,
     height: 600,
   });
+
+  // Option 1
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  // Option 2
+  mainWindow.loadFile(`./app/index.html`);
 }
 
 app.on("ready", createMainWindow);
